b5755728c78e3cb5eea993afdac18b05098c8211
Omole Oluwafemi <omostica2020@gmail.com>
2022-09-08 19:13:39 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
- * main - print the string in the put output
- *
- * Description: using the main function
- * this programe prints "programming is like building a multilingual puzzle
- * Return: 0
+ * Description: main - print out a string to stdout
+ * Retur: 0 if success.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"programming is like building a multilingual puzzle"):
+	return (0):
 }
 
