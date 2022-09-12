1d59b62cb42b0389ada200a450e18f3def321fcd
Omole Oluwafemi <omostica2020@gmail.com>
2022-09-12 15:48:48 -0700

    proper grammer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - Prints a text
  *
- * Description: using the main function
- * this program points "programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 { 
-	printf (with proper grammer, but the outcome is a piece of art,\n");
-	return (0)
+	printf ("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
