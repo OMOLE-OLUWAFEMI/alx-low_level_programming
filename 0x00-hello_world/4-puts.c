712b34fbcee7789893be2c77ac7bfa2ee9380d4c
Omole Oluwafemi <omostica2020@gmail.com>
2022-09-12 15:42:17 -0700

    programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main - print out a string to stdout
- * Retur: 0 if success.
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"):
-	return (0):
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
-
