2d0730ee3f5750a30d6d283fd1885d0f1030f789
Omole Oluwafemi <omostica2020@gmail.com>
2022-09-14 03:53:59 -0700

    beginning

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,10 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriatly.
+ * On error, -1 is returned, and errno is set appropiatly.
  */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+
 }
-
-
