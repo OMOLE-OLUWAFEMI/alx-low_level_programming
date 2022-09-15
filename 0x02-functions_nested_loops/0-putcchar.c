a6c83f57f089a23d76a59ab9f97cc3a0e483894f
Omole Oluwafemi <omostica2020@gmail.com>
2022-09-14 19:39:41 -0700

    0-putcchar.c

--- 0x02-functions_nested_loops/0-putcchar.c
+++ 0x02-functions_nested_loops/0-putcchar.c
@@ -3,4 +3,12 @@
 /**
 * _putchar - writes the character c to stdout
 * @c: The charcter
+*
+* Return: on sucess 1
+* On error, -1 is returned, and errno is set appropiately.
+*/
+int _putchar(char c)
+{
+	return (wites(1, &c, 1));
+}
 
