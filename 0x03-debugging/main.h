eb6ce405c1929cf06261636ebc9186781a6419ac
Omole Oluwafemi <omostica2020@gmail.com>
2022-09-15 15:58:28 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MATH_H
+#define MATH_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_days(int, int);
